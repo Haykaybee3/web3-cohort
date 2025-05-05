d28c67f1c58e594c5c332ed7f791c3b525458bd4
surprise <haykaybee3@gmail.com>
2025-05-05 13:23:14 +0100

    styling feature

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        bounce: {
+          '0%, 100%': { transform: 'translateY(-5%)' },
+          '50%': { transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        bounce: 'bounce 1s infinite',
+      }
+    },
+  },
+  plugins: [],
+}
